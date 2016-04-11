fe060f0a3ccd0844f4815679f78c6df3668740d0
Philipp Hoefflin <philipp.hoefflin@haufe-lexware.com>
2016-04-11 17:17:08 +0200

    kill_application_frame_host.bat: doku

--- win/kill_application_frame_host.bat
+++ win/kill_application_frame_host.bat
@@ -1,12 +1,11 @@
 @echo off
 REM
-REM Kill Application Frame Host which causes shortcut delays
 REM kill ApplicationFrameHost.exe, which ist automatically started for example by the
 REM calculator app and sometimes causes several second hangs when usimg windows
-REM shortcuts (like starten a Terminal).
+REM shortcuts (like starting a Terminal).
 REM
-REM This will also kill applications using it (like calculator) but that's ok with me 
-REM it will automatically get restartet on the next APP startup.
+REM This will also kill applications using it (like calculator) but that's ok with me,
+REM the ApplicationFrameHost.exe will automatically get restartet on the next APP startup.
 REM
 REM (see also: https://superuser.com/questions/426947/slow-windows-desktop-keyboard-shortcuts)
 REM
