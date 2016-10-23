a0a07611b740f2de758cc95ac138b0a31641b2e4
Philipp Hoefflin <p.hoefflin@posteo.de>
2016-10-23 20:37:49 +0200

    added offlineimap, minor changes on mutt/mail config

--- .bashrc.d/99-mail.bash
+++ .bashrc.d/99-mail.bash
@@ -1,7 +1,29 @@
 # basic settings for mail and friends
 $_bashrc_debug_echo "in ${BASH_SOURCE[0]}"
 
+# check for errors in error logs of all running offlineimap processes:
+$_bashrc_debug_echo "in ${BASH_SOURCE[0]}: checking offlineimap logs"
+for pid in $(pgrep offlineimap); do
+	# get logfile name from process' open file descriptors
+	logs=$(ls -l /proc/$pid/fd/*| egrep '.log$' | awk '{ print $NF}')
+	# should be just one, but just to be sure:
+	for log in $logs; do
+		errors="$(grep -i "error" $log| tail -10)"
+		if [ -n "$errors" ]; then
+			echo "*************************************************************"
+			echo "| WARNING:"
+			echo "| Errors found in $log, please check, last messages:"
+			echo "|-----------------------------------------------------------"
+			echo "$errors" | sed 's/^/| ... /'
+			echo "*************************************************************"
+			echo
+		fi
+	done
+done
+unset pid logs log errors
+
 # if msmtp-queue is installed: check for unsent mail
+$_bashrc_debug_echo "in ${BASH_SOURCE[0]}: checking unsent messages in msmtp queue"
 if [ -n "$(type -p msmtp-queue)" ]; then
 	queue_info=$(msmtp-queue)
 	case "$queue_info" in
@@ -16,4 +38,6 @@ if [ -n "$(type -p msmtp-queue)" ]; then
 		echo "*************************************************"
 		;;
 	esac
+	unset queue_info
 fi
+
